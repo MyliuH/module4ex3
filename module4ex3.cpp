b7b82270e1813ce51ef9e8deecdafd2760f9ff56
MyliuH <138070641+MyliuH@users.noreply.github.com>
2024-08-11 06:05:04 +0300

    Add files via upload

---
+++ module4ex3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		std::cout << "*";
+		for (int j = 0; j < i; j++) {
+			std::cout << " ";
+		}
+	std::cout << "*" << std::endl;
+	};
+	for (int i = 0; i < n; i++) {
+		std::cout << "*";
+	}
+}
+
